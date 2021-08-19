7f17e1582f9e8bd1c50a19a4d224f0503af7afc3
javierpb275 <javierpb275@gmail.com>
2021-08-19 17:04:37 +0200

    used path package that comes with node to sprecified the directory. created public folder and index.html to show that html when our app is running

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.json());
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.json(), express.static(publicDirectoryPath));
 
 module.exports = app;
