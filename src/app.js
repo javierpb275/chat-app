38e4a55082b5978f125792fce5d4259911d0b866
javierpb275 <javierpb275@gmail.com>
2021-08-19 17:31:01 +0200

    installed socket.io and added configurationand. Also used http package from node so we can add the configuration for socket.io

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
 
@@ -7,4 +9,12 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.json(), express.static(publicDirectoryPath));
 
-module.exports = app;
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+io.on("connection", () => {
+  console.log("New WebSocket connection");
+});
+
+module.exports = server;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const app = require("./app");
+const server = require("./app");
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
