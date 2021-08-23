1fa59973bd52a2de080d8ac2eebc6580c83f4f6e
javierpb275 <javierpb275@gmail.com>
2021-08-23 15:53:09 +0200

    installed  bad-words and removed comments

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,8 +1,5 @@
 const socket = io();
 
-// server (emit) -> client (receive) --acknowledgement--> server
-// client (emit) -> server (receive) --acknowledgement--> client
-
 socket.on("message", (message) => {
   console.log(message);
 });
@@ -11,7 +8,7 @@ document.querySelector("#message-form").addEventListener("submit", (e) => {
   e.preventDefault();
   const message = e.target.elements.message.value;
   socket.emit("sendMessage", message, (message) => {
-    console.log('The message was delivered', message);
+    console.log("The message was delivered", message);
   });
 });
 
