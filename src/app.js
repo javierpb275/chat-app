76b0e63ccd9aab9dd90b77501a9fd40f344cf7b2
javierpb275 <javierpb275@gmail.com>
2021-08-23 14:14:08 +0200

    broadcasting events

--- src/app.js
+++ src/app.js
@@ -13,12 +13,18 @@ const server = http.createServer(app);
 
 const io = socketio(server);
 
-io.on("connection", (socket) => {
-  socket.emit("message", "WELCOME!");
+io.on("connection", (socket) => {//run code when a given client connected
+  socket.emit("message", "WELCOME!");//emit to that particular connection
+
+  socket.broadcast.emit('message', 'A new user has joined!')//emit to everybody but that particular connection
 
   socket.on("sendMessage", (message) => {
-    io.emit("message", message);
+    io.emit("message", message);//send to everyone
   });
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left');//when user disconnect, send message to  other users connected
+  })
 });
 
 module.exports = server;
