36f16ec75c886efea66e8ea8f4c9959696cf53d7
javierpb275 <javierpb275@gmail.com>
2021-08-23 14:34:27 +0200

    sharing your location

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,3 +9,16 @@ document.querySelector("#message-form").addEventListener("submit", (e) => {
   const message = e.target.elements.message.value;
   socket.emit("sendMessage", message);
 });
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolacation is not supported by your browser");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit("sendLocation", {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  });
+});
