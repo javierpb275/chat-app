3481f81860818e3a97760b48650b9375b76d3257
javierpb275 <javierpb275@gmail.com>
2021-08-27 18:56:38 +0200

    sockect.io rooms

--- src/app.js
+++ src/app.js
@@ -19,21 +19,29 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", (socket) => {
-  socket.emit("message", generateMessage("WELCOME!"));
 
-  socket.broadcast.emit("message", generateMessage("A new user has joined!"));
+  socket.on("join", ({ username, room }) => {
+
+    //socket.emit, io.emit, socket.broadcast.emit
+    //io.to.emit, socket.broadcast.to.emit
+
+    socket.join(room);
+    socket.emit("message", generateMessage("WELCOME!"));
+    socket.broadcast.to(room).emit("message", generateMessage(`${username} has joined!`));
+
+  });
 
   socket.on("sendMessage", (message, callback) => {
     const filter = new Filter();
     if (filter.isProfane(message)) {
       return callback("Profanity is not allowed!");
     }
-    io.emit("message", generateMessage(message));
+    io.to('room').emit("message", generateMessage(message));
     callback();
   });
 
   socket.on("sendLocation", (coords, callback) => {
-    io.emit(
+    io.to('room').emit(
       "locationMessage",
       generateLocationMessage(
         `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
