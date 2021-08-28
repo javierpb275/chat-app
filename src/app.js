f229b2afa0ce4a19d5ecbc00c06cc4b281330a51
javierpb275 <javierpb275@gmail.com>
2021-08-28 17:09:14 +0200

    rendering user list

--- src/app.js
+++ src/app.js
@@ -38,6 +38,10 @@ io.on("connection", (socket) => {
         "message",
         generateMessage("Admin", `${user.username} has joined!`)
       );
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      })
     callback();
   });
 
@@ -70,6 +74,10 @@ io.on("connection", (socket) => {
         "message",
         generateMessage("Admin", `${user.username} has left!`)
       );
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      })
     }
   });
 });
